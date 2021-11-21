a29000e7352963ddc24cd50e8b3ca5023f48ef19
AngieCadena <as.cadena10@uniandes.edu.co>
2021-11-21 17:10:39 -0500

    primer comit

---
+++ backend/database/db.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI:
+    "mongodb+srv://AngieCadena:Profecion123@profecion.p53k5.mongodb.net/baseDatosProfecion?retryWrites=true&w=majority",
+    secretOrKey:"secret",
+};
